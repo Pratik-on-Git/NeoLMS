21192de78d5c82b7d78fc802485accdc02f563a6
Pratik Das <pratikdasmartingarrix@gmail.com>
2026-02-02 17:25:09 +0530

    fix: update baseURL to use env variable for better configuration

--- lms-project/lib/auth-client.ts
+++ lms-project/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
+import { env } from "./env";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [
     emailOTPClient(),
     adminClient(),
